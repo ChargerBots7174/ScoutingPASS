a7c2bd2e3cfd39dc8762f7a9e0921683fcda9a3b
ChargerBots7174 <98183917+ChargerBots7174@users.noreply.github.com>
2023-03-13 18:41:53 -0400

    WAYNE PRACTICE

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2022midtr",
+      "defaultValue": "2023midtr",
       "required": "true",
       "disabled": "true"
     },
