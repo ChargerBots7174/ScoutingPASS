fa19764eb16b55b831aeca3fbe5d0cfb8c086ede
ChargerBots7174 <98183917+ChargerBots7174@users.noreply.github.com>
2023-03-17 10:40:01 -0400

    BELLEVILLE

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023midtr",
+      "defaultValue": "2023mibel",
       "required": "true",
       "disabled": "true"
     },
