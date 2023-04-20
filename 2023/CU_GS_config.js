92ac541f01a48c45adfc9b2751092af9b0c54f69
ChargerBots7174 <98183917+ChargerBots7174@users.noreply.github.com>
2023-04-20 08:09:16 -0400

    WORLDS P2

--- 2023/CU_GS_config.js
+++ 2023/CU_GS_config.js
@@ -17,7 +17,7 @@ var config_data = `
       "code": "e",
       "gsCol": "event",
       "type": "event",
-      "defaultValue": "2023ARC",
+      "defaultValue": "2023CMPTX",
       "required": "true",
       "disabled": "true"
     },
